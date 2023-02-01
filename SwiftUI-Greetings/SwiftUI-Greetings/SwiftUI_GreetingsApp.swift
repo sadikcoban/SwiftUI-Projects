db295dd31259dd54b5ad9c3eeae620a53feccb6b
sadikcoban <coban.sadik@icloud.com>
2023-02-01 19:08:19 +0300

    first commit

---
+++ SwiftUI-Greetings/SwiftUI-Greetings/SwiftUI_GreetingsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_GreetingsApp.swift
+//  SwiftUI-Greetings
+//
+//  Created by Sadık Çoban on 1.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_GreetingsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

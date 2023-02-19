ac8d5ab090c28d83483d0d83656f401f8e8dbb36
sadikcoban <coban.sadik@icloud.com>
2023-02-19 20:07:12 +0300

    create swiftui basics

---
+++ SwiftUI Basics/SwiftUI Basics/SwiftUI_BasicsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_BasicsApp.swift
+//  SwiftUI Basics
+//
+//  Created by Sadık Çoban on 19.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_BasicsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
